0563675ed0073cb1b62cd70938e2107c94ff2286
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2024-02-18 22:22:00 +0200

    + Add category nav block

--- src/components/CategoryNav/CategoryNav.jsx
+++ src/components/CategoryNav/CategoryNav.jsx
@@ -1,5 +1,24 @@
+// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 import "./CategoryNav.scss";
 
 export default function CategoryNav() {
-  return <div>Category Nav</div>;
+  const { data } = useFetch("./api/cameras.json");
+  const allCategories = data?.map((item) => item.categories);
+  const uniqueCategories = [...new Set(allCategories)];
+
+  console.log(uniqueCategories);
+  return (
+    <aside className="nav">
+      <div className="nav-title">BROWSE CATEGORIES</div>
+      <div className="nav-categories">
+        {uniqueCategories.map((category) => (
+          <Link to={`products/${category}`} className="nav-category" key={category}>
+            {category}
+          </Link>
+        ))}
+      </div>
+    </aside>
+  );
 }
