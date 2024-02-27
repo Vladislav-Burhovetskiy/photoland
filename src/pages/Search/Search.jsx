0073896ec9fc8b18111c1445b4bb4c1346f5526a
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2024-02-27 20:56:32 +0200

    + Add title for search result and no result

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -15,15 +15,23 @@ export default function Search() {
     product.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const resultTitle =
+    searchCameras?.length > 0
+      ? `${searchCameras.length} results for ${searchTerm}`
+      : `No results found for ${searchTerm}`;
+
   return (
     <div className="search container">
       <div className="search-category">
         <CategoryNav />
       </div>
-      <div className="search-cameras">
-        {searchCameras?.map((product) => (
-          <Product key={product.id} camera={product} />
-        ))}
+      <div className="search-result">
+        <h2 className="search-result__title">{resultTitle}</h2>
+        <div className="search-result__cameras">
+          {searchCameras?.map((product) => (
+            <Product key={product.id} camera={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
