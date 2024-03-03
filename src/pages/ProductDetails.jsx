bba4aa38f7edec43d56138b4bfdbbc8fe2a0ebc3
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2024-03-03 14:04:18 +0200

    + Add go back btn and finish product details styles

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import RelatedProduct from "../components/RelatedProduct/RelatedProduct";
 // import { useCartIsOpen } from "../../hooks/useCartIsOpen";
 import { useDataContext } from "../hooks/useDataContext";
@@ -6,6 +6,7 @@ import SpinnerLoading from "../components/Spinner/SpinnerLoading";
 import "./ProductDetails.scss";
 
 export default function ProductDetails() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { data, isLoading } = useDataContext();
 
@@ -18,28 +19,33 @@ export default function ProductDetails() {
   });
 
   return (
-    <section className="product-details container">
-      <div className="product-details__photo-container">
-        <img
-          className="product-details__photo-image"
-          src={camera?.image}
-          alt={camera?.title}
-        />
+    <section className="container">
+      <div className="product-details">
+        <button className="product-details__back-nav"onClick={() => navigate(-1)}>⟵ go back</button>
+        <div className="product-details__photo-container">
+          <img
+            className="product-details__photo-image"
+            src={camera?.image}
+            alt={camera?.title}
+          />
+        </div>
+        <div className="product-details__info">
+          <p className="product-details__category">
+            {camera?.categories} cameras
+          </p>
+          <h3 className="product-details__title">{camera?.title}</h3>
+          <ul className="product-details__description">
+            {camera?.description.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+          <div className="product-details__order">
+            <p className="product-details__order-price">${camera?.price}</p>
+            <button>Add to cart</button>
+          </div>
+        </div>
       </div>
-      <div className="product-details__info">
-        <p className="product-details__category">
-          {camera?.categories} cameras
-        </p>
-        <h3 className="product-details__title">{camera?.title}</h3>
-        <ul className="product-details__description">
-          {camera?.description.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-        <p className="product-details__price">${camera?.price}</p>
-        <button>Add to cart</button>
-        <RelatedProduct />
-      </div>
+      <RelatedProduct />
     </section>
   );
 }
